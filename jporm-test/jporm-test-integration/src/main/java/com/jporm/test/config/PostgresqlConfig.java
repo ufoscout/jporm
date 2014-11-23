d93336dc70d354fee85708d2ee9981056421d3a9
ufoscout <ufoscout@yahoo.it>
2014-11-23 23:42:22 +0100

    Postgresql tests use liquibase

--- jporm-test/jporm-test-integration/src/main/java/com/jporm/test/config/PostgresqlConfig.java
+++ jporm-test/jporm-test-integration/src/main/java/com/jporm/test/config/PostgresqlConfig.java
@@ -17,6 +17,8 @@ package com.jporm.test.config;
 
 import javax.sql.DataSource;
 
+import liquibase.integration.spring.SpringLiquibase;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -35,34 +37,43 @@ public class PostgresqlConfig {
 	public static final String DATASOURCE_NAME = "POSTGRESQL.DataSource";
 	public static final String TRANSACTION_MANAGER_NAME = "POSTGRESQL.TransactionManager";
 	public static final String DB_DATA_NAME = "POSTGRESQL.DA_DATA";
+	public static final String LIQUIBASE_BEAN_NAME = "POSTGRESQL.LIQUIBASE";
 
 	@Autowired
 	private Environment env;
 
 	@Bean(name={DATASOURCE_NAME})
-	public DataSource getH2DataSource() {
+	public DataSource getDataSource() {
 
 		DataSource dataSource = BuilderUtils.buildDataSource(DB_TYPE, env);
 
 		ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
 		databasePopulator.setIgnoreFailedDrops(true);
 		databasePopulator.addScript(new ClassPathResource("/sql/postgresql_drop_db.sql"));
-		databasePopulator.addScript(new ClassPathResource("/sql/postgresql_create_db.sql"));
+		//databasePopulator.addScript(new ClassPathResource("/sql/postgresql_create_db.sql"));
 		DatabasePopulatorUtils.execute(databasePopulator, dataSource);
 
 		return dataSource;
 	}
 
 	@Bean(name=TRANSACTION_MANAGER_NAME)
-	public DataSourceTransactionManager getH2DataSourceTransactionManager() {
+	public DataSourceTransactionManager getDataSourceTransactionManager() {
 		DataSourceTransactionManager txManager = new DataSourceTransactionManager();
-		txManager.setDataSource(getH2DataSource());
+		txManager.setDataSource(getDataSource());
 		return txManager;
 	}
 
 	@Bean(name=DB_DATA_NAME)
 	public DBData getDBData() {
-		return BuilderUtils.buildDBData(DB_TYPE, env, getH2DataSource(), getH2DataSourceTransactionManager());
+		return BuilderUtils.buildDBData(DB_TYPE, env, getDataSource(), getDataSourceTransactionManager());
 	}
 
+	@Bean(name=LIQUIBASE_BEAN_NAME)
+	public SpringLiquibase getSpringLiquibase() {
+		SpringLiquibase liquibase = new SpringLiquibase();
+		liquibase.setDataSource(getDataSource());
+		liquibase.setChangeLog("file:../jporm-test-integration/liquibase/liquibase-0.0.1.xml");
+		//liquibase.setContexts("development, production");
+		return liquibase;
+	}
 }
