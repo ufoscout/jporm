51e04a3aacc4b43e82795d8152a8a1b074ea83a0
ufoscout <ufoscout@yahoo.it>
2014-12-22 10:30:30 +0100

    Regression test moved into core

--- jporm-core/src/test/java/com/jporm/core/session/datasource/DataSourceSessionProviderTest.java
+++ jporm-core/src/test/java/com/jporm/core/session/datasource/DataSourceSessionProviderTest.java
@@ -13,7 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
-package com.jporm.vertx3;
+package com.jporm.core.session.datasource;
+
+import static org.junit.Assert.assertNotNull;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
@@ -23,54 +25,14 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.jporm.BaseVertxTestApi;
+import com.jporm.core.BaseTestApi;
+import com.jporm.core.domain.People;
 import com.jporm.session.Session;
-import com.jporm.session.TransactionCallback;
-import com.jporm.session.TransactionCallbackVoid;
 
-public class JPOSessionTest extends BaseVertxTestApi {
+public class DataSourceSessionProviderTest extends BaseTestApi {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 
-	@Test
-	public <T> void testJPOSession() throws InterruptedException {
-		com.jporm.JPO jpo = getJPO();
-
-		final Long id = jpo.session().doInTransaction(new TransactionCallback<Long>() {
-			@Override
-			public Long doInTransaction(final Session session) {
-				//				Long _id = create(session, "");
-				//				session.find(People.class, _id).get();
-				//				return _id;
-
-
-				long _id = create(session, "");
-				logger.info("Saved people with id [{}] and name [{}]", _id, "");
-				People people = session.find(People.class, _id).get();
-				assertNotNull(people);
-				return _id;
-
-			}
-		});
-
-		Thread thread = new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				jpo.session().doInTransactionVoid(new TransactionCallbackVoid() {
-					@Override
-					public void doInTransaction(final Session session) {
-						People found = session.find(People.class, id).get();
-						logger.info("Found: " + found);
-						assertNotNull(found);
-					}
-				});
-			}});
-
-		thread.start();
-		thread.join();
-
-	}
 
 	@Test
 	public <T> void testJPOWithLambdaSession() throws InterruptedException {
@@ -84,18 +46,11 @@ public class JPOSessionTest extends BaseVertxTestApi {
 			return _id;
 		});
 
-		final BlockingQueue<People> queue = new ArrayBlockingQueue<People>(10);
-		new Thread(() -> jpo.session().doInTransactionVoid(session -> {
+		jpo.session().doInTransactionVoid(session -> {
 			People found = session.find(People.class, id).get();
 			logger.info("Found: " + found);
-			try {
-				queue.put(found);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		})).start();
-
-		assertNotNull( queue.poll(2, TimeUnit.SECONDS) );
+			assertNotNull( found );
+		});
 
 	}
 
@@ -116,4 +71,5 @@ public class JPOSessionTest extends BaseVertxTestApi {
 		return people.getId();
 
 	}
+
 }
