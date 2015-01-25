ec263c306fe1eed8fc686f66ad4bfdf3371ab75a
ufoscout <ufoscout@yahoo.it>
2015-01-25 21:41:15 +0100

    Fixed liquibase script to work with MySQL

--- jporm-test/jporm-test-integration/src/main/java/com/jporm/test/config/OracleConfig.java
+++ jporm-test/jporm-test-integration/src/main/java/com/jporm/test/config/OracleConfig.java
@@ -24,11 +24,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.core.env.Environment;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
-import org.springframework.transaction.PlatformTransactionManager;
 
 import com.jporm.core.dialect.DBType;
 
